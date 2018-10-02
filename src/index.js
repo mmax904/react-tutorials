3e35793f7a2c52aed8c99386c1354058a9ee48c1
Manish Mahant <manishmahant>
2018-10-02 21:15:15 +0530

    synchronous

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import rootReducer from './syncronous/reducers'
-import App from './syncronous/components/App'
+import rootReducer from './synchronous/reducers'
+import App from './synchronous/components/App'
 const store = createStore(rootReducer)
 render(
   <Provider store={store}>
